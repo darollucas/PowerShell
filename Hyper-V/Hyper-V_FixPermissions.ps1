b4c703d90c371c9938bd471f335adeada6a9df30
Darol Lucas <76235720+darollucas@users.noreply.github.com>
2023-07-08 21:32:46 -0700

    Update Hyper-V_FixPermissions.ps1

--- Hyper-V/Hyper-V_FixPermissions.ps1
+++ Hyper-V/Hyper-V_FixPermissions.ps1
@@ -1,3 +1,30 @@
+<#
+.SYNOPSIS
+This PowerShell script modifies file permissions for Hyper-V related files, and creates a batch file to repair Hyper-V folder permissions.
+
+.DESCRIPTION
+This script first defines a variable containing the commands for a batch file intended to reset permissions on the Hyper-V directory. It then writes this batch file to the directory containing the PowerShell script.
+
+The script retrieves a list of all .vhd and .vhdx files within the Hyper-V directory. For each of these files, it retrieves the existing Access Control List (ACL) and creates a new access rule granting specific permissions to the Virtual Machine (VM) associated with the file. The VM's Security Identifier (SID) is constructed using the name of the directory containing the file, which is assumed to match the VM's identifier.
+
+Finally, the script adds the new access rule to the file's ACL, effectively granting the VM the specified permissions on the file.
+
+The batch file and the permission updates allow for more secure and functional operation of Hyper-V VMs.
+
+.COMPATIBILITY
+This script requires PowerShell 3.0 or newer due to its use of cmdlets such as Get-ChildItem with the -File switch, and the $PSScriptRoot automatic variable. It has been designed specifically for use with Hyper-V on Windows Server 2012 and later.
+
+.NOTES
+Script Name: RepairHVPermissions.ps1
+Created By: TechBase IT
+Version: 1.0
+
+.EXAMPLE
+.\RepairHVPermissions.ps1
+
+This will create the RepairHVPermissions.bat file in the same directory as the PowerShell script,
+and update permissions on all .vhd and .vhdx files in the Hyper-V directory.
+#>
 $HyperVFolder = "C:\HyperV"  # Specify the Hyper-V folder path
 
 # Define the repair script content
